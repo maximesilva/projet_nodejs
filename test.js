c585437672e69e9b0a61249adcae80cc1370c8fe
maximesilva <maxime.silva@le-campus-numerique.fr>
2020-04-09 13:48:12 +0200

    querystring et selection des parametre de l'url avec le .parse et .query

--- test.js
+++ test.js
@@ -1,21 +1,15 @@
 var http = require('http');
 var url = require('url');
+var querystring = require('querystring');
 
 var server = http.createServer(function(req, res) {
-    var page = url.parse(req.url).pathname; // decoupe l'url pour avoir la page demandé
-    console.log(page);
+    var params = querystring.parse(url.parse(req.url).query);//on récupère les parametre passé dans l'url
     res.writeHead(200, {"Content-Type": "text/plain"});
-    if (page == '/') {
-        res.write('Vous êtes à l\'accueil, que puis-je pour vous ?');
+    if ('prenom' in params && 'nom' in params) {
+        res.write('Vous vous appelez ' + params['prenom'] + ' ' + params['nom']);
     }
-    else if (page == '/sous-sol') {
-        res.write('Vous êtes dans la cave à vins, ces bouteilles sont à moi !');
-    }
-    else if (page == '/etage/1/chambre') {
-        res.write('Hé ho, c\'est privé ici !');
-    }else{
-        res.writeHead(404); //permet d'envoyer le code d'erreur 404
-        res.write('404');
+    else {
+        res.write('Vous devez bien avoir un prénom et un nom, non ?');
     }
     res.end();
 });
