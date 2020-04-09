3ba6e047887f83740e44875a42425f43dff96505
maximesilva <maxime.silva@le-campus-numerique.fr>
2020-04-09 13:31:25 +0200

    require url, parse l'url, code erreur envoyer au navigateur

--- test.js
+++ test.js
@@ -1,4 +1,27 @@
-//require effectue un appel à une bibliothèque de Node.js, ici la bibliothèque "http"
+var http = require('http');
+var url = require('url');
+
+var server = http.createServer(function(req, res) {
+    var page = url.parse(req.url).pathname; // decoupe l'url pour avoir la page demandé
+    console.log(page);
+    res.writeHead(200, {"Content-Type": "text/plain"});
+    if (page == '/') {
+        res.write('Vous êtes à l\'accueil, que puis-je pour vous ?');
+    }
+    else if (page == '/sous-sol') {
+        res.write('Vous êtes dans la cave à vins, ces bouteilles sont à moi !');
+    }
+    else if (page == '/etage/1/chambre') {
+        res.write('Hé ho, c\'est privé ici !');
+    }else{
+        res.writeHead(404); //permet d'envoyer le code d'erreur 404
+        res.write('404');
+    }
+    res.end();
+});
+server.listen(8080);
+
+/*require effectue un appel à une bibliothèque de Node.js, ici la bibliothèque "http"
 var http = require('http');
 
 //req contient les infos que le visiteur a demandé et res la reponse qu'on renvoi
@@ -22,4 +45,4 @@ var server = http.createServer(function(req, res) { //req contient les infos que
     res.end();
 });
 //lancement du server et ecoute sur le port 8080
-server.listen(8080);
+server.listen(8080);*/
