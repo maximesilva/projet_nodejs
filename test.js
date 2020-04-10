a89e073f35cf59badec89e3d1cb32f577e424c5f
maximesilva <maxime.silva@le-campus-numerique.fr>
2020-04-10 11:16:14 +0200

    last commit

--- test.js
+++ test.js
@@ -1,8 +1,26 @@
-var markdown = require('markdown').markdown;
+const fs = require('fs');
+const folder = require('os').homedir();
 
-console.log(markdown.toHTML('Un paragraphe en **markdown** !'));
+function getIcon(item) {
+    if (item.isDirectory()) { //du module fs/file system
+        return '📁'; //si c'est un dossier
+    }
+    return '🌷';
+}
 
-/*let monmodule = require('./monmodule'); //j'appelle mon module que j'ai créer
+async function list(path) {
+    const items = await fs.promises.readdir(path, {withFileTypes: true});
+    for (const item of items) {
+        console.log(getIcon(item) + ' ' + item.name);
+    }
+    return items.length;
+}
 
-monmodule.direBonjour(); // j'appelle mes fonctions de mon module
-monmodule.direByeBye();*/
+list(folder)
+
+    .then(itemCount => {
+        console.error(`Done! The folder "${folder}" contains ${itemCount} items`);
+    })
+    .catch(error => {
+        console.error(`Cannot list the folder "${folder}"`, error);
+    });
