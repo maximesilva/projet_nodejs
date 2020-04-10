74c8d0b1e5ea6940ca46220da6dc1f17aa4a46c1
maximesilva <maxime.silva@le-campus-numerique.fr>
2020-04-10 09:59:48 +0200

    ajout module markdown, declaration monmodule dans package.json

--- test.js
+++ test.js
@@ -1,4 +1,8 @@
-let monmodule = require('./monmodule'); //j'appelle mon module que j'ai créer
+var markdown = require('markdown').markdown;
+
+console.log(markdown.toHTML('Un paragraphe en **markdown** !'));
+
+/*let monmodule = require('./monmodule'); //j'appelle mon module que j'ai créer
 
 monmodule.direBonjour(); // j'appelle mes fonctions de mon module
-monmodule.direByeBye();
+monmodule.direByeBye();*/
